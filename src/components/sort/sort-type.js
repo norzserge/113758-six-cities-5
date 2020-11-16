91b750e4ce64c1afaaaab8e5655964ff5ad6736b
norzserge <metelskiisa@mail.ru>
2020-11-17 01:28:13 +0300

    fix bugs with proptypes

--- src/components/sort/sort-type.js
+++ src/components/sort/sort-type.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export const sortType = {
   currentCityData: PropTypes.array.isRequired,
-  getFilteredCityData: PropTypes.array.isRequired,
+  getFilteredCityData: PropTypes.func.isRequired,
   filterValue: PropTypes.string.isRequired,
   setFilterValue: PropTypes.func.isRequired,
   filterIndex: PropTypes.number.isRequired,
